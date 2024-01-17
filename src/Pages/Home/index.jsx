427768742571942bffebfaa65ccba3edf6719748
Sargis Abrahamyan <118532124+Sargis-Abrahamyan@users.noreply.github.com>
2024-01-18 00:02:03 +0400

    Update home.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,23 +1,9 @@
-import { useState, useEffect } from 'react';
-
 import doctor from '../../Assets/image/doctor.jpg';
 import { homeHelloWorldText } from '../../Constants/constatns';
-import styles from './home.module.css';
 import Watch from './Watch/Watch';
+import styles from './home.module.css';
 
 const Home = () => {
-    const [time, setTime] = useState(null);
-
-    useEffect(() => {
-        const timeInterval = setInterval(() => {
-            setTime(new Date().toLocaleTimeString());
-        }, 1000);
-
-        return () => clearInterval(timeInterval);
-    }, []);
-
-
-    console.log(time);
     return (
         <div className={styles.home_container}>
             <div className={styles.content_body}>
